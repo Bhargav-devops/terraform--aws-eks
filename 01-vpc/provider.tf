e99f84eaff9897578cb2e9f96ab0cbc638695504
Bhargav-devops <bhargav.devopsaws@gmail.com>
2025-03-21 11:45:39 +0530

    terraform aws eks

---
+++ 01-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "bhargav-bucket-dev"
+    key    = "vpc"
+    region = "us-east-1"
+    dynamodb_table = "bhargav-locking-dev"
+  }
+
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
